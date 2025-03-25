e51e999c53ca7e853d1108c5c67c9a9946eaf7ad
tobyguo <tobyguo@tencent.com>
2025-03-25 18:32:03 +0800

    feat: update early-init.el

--- conf/init-view.el
+++ conf/init-view.el
@@ -21,9 +21,6 @@
 (require 'popup)
 (setq clippy-tip-show-function #'clippy-popup-tip-show)
 
-;; 隐藏状态栏
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
 
 ;直接emacs命令打开的窗口相关设置,不要在这里设置字体，否则daemon 启动时字体有可能没创建好，会导致字体设置失败
 (setq-default window-system-default-frame-alist
--- early-init.el
+++ early-init.el
@@ -15,50 +15,57 @@
 
 (defconst my/start-time (current-time))
 
+;; 临时禁用调试工具（移除 `mt` 宏)
+(setq load-prefer-newer t
+      message-log-max 16384
+      gc-cons-threshold (* 400 1024 1024)    ; 400MB（平衡内存与启动速度）
+      gc-cons-percentage 0.6
+      read-process-output-max (* 3 1024 1024))
+
+
+;; ==== UI 配置 ====
+;; 禁用非必要组件
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(when (fboundp 'menu-bar-mode) (menu-bar-mode 0))
+
+(defvar file-name-handler-alist-old file-name-handler-alist)
+
+;; ==== GC 恢复 ====
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (* 16 1024 1024)  ; 恢复为 16MB
+                  gc-cons-percentage 0.1
+                  file-name-handler-alist file-name-handler-alist-old)
+
+            (garbage-collect)
+            (message "Load time %.06f" (float-time (time-since my/start-time)))
+            (message "Emacs ready in %.2fs (RAM: %.2fMB)"
+                     (float-time (time-since my/start-time))
+                     (/ (car (memory-usage)) 1024.0 1024.0))))
+
+;; ========================================================
+;; ========================================================
+
 (defmacro mt (&rest body)
   "Measure the time it takes to evaluate BODY."
   `(let ((time (current-time)))
      ,@body
      (message "%.06f" (float-time (time-since time)))))
 
-(defvar file-name-handler-alist-old file-name-handler-alist)
-
-(setq file-name-handler-alist nil
-      message-log-max 16384
-      gc-cons-threshold most-positive-fixnum   ;; Defer Garbage collection
-      gc-cons-percentage 1.0)
-
-(add-hook 'window-setup-hook
-          (lambda ()
-            (setq file-name-handler-alist file-name-handler-alist-old
-                  gc-cons-threshold (* 64 1024 1024)
-                  gc-cons-percentage 0.1)
-            (garbage-collect)
-            (message "Load time %.06f" (float-time (time-since my/start-time))))
-          t)
-
-;; (setq-default
-;;  ;; user-full-name ""                ;记得改成你的名字
-;;  ;; user-login-name "jixiuf"
-;;  user-mail-address (concat "jixiuf" "@" "qq.com"))
 (setq load-prefer-newer t)              ;当 el 文件比 elc 文件新的时候,则加载 el,即尽量 Load 最新文件文件
 ;; By default Emacs will initiate GC every 0.76 MB allocated (gc-cons-threshold == 800000).
 ;; If we increase this to 20 MB (gc-cons-threshold == 20000000) we get:
 (setq read-process-output-max (* 3 1024 1024)) ;; 3mb default 4k
 
-;; 禁用工具栏，滚运条 菜单栏
-(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
 (setq-default default-frame-alist initial-frame-alist)
 
 (setq-default mode-line-format nil)
 (setq mode-line-format nil)
 (setq modus-themes-hl-line  '(intense accented))
-(load "~/.emacs.d/init-base.el")
+
+(load-file "~/.emacs.d/init-base.el")
 (when (eq system-type 'darwin) (require 'conf-macos))
 (when (eq system-type 'gnu/linux) (require 'conf-linux))
 (when (eq system-type 'windows-nt) (require 'conf-w32))
-
-;; (setq modus-themes-tabs-accented nil)
-;; 几个不错的颜色 https://www.zhihu.com/question/497851000/answer/2249878929
