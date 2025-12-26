fa2524d77594c460db43ee4127d7a81d9cbce02a
tobyguo <tobyguo@tencent.com>
2025-12-26 17:26:06 +0800

    conf-marco.el

--- conf/conf-macro.el
+++ conf/conf-macro.el
@@ -1,12 +1,4 @@
 ;;; -*- lexical-binding: t; -*-
-(defmacro vmacs-define-key (mode-map key cmd  &optional  feature state)
-  "define-key in `eval-after-load' block. `feature' is the file name where defined `mode-map'"
-  (if state
-      (if feature
-          `(with-eval-after-load ,feature
-             (with-eval-after-load 'evil  (evil-define-key ,state ,mode-map ,key ,cmd)))
-        `(with-eval-after-load 'evil  (evil-define-key ,state ,mode-map ,key ,cmd)))
-    `(with-eval-after-load ,feature (define-key ,mode-map ,key ,cmd))))
 
 (defvar vmacs-leader-mode-map (make-sparse-keymap) "High precedence keymap.")
 (defvar vmacs-space-leader-mode-map (make-sparse-keymap) "High precedence keymap.")
@@ -26,31 +18,6 @@
   `(define-key vmacs-space-leader-mode-map (kbd ,key) ,cmd))
 
 
-;; (print (macroexpand-1 '(with-mode-off icomplete-vertical-mode  (find-file-at-point))))
-(defmacro with-mode-off (mode &rest body)
-  (declare (indent defun)
-           (doc-string 3))
-  (macroexp-let2 nil mode-p `(bound-and-true-p ,mode)
-    `(progn
-       (when ,mode-p (,mode -1))
-       (unwind-protect
-           (progn ,@body)
-         (when ,mode-p (,mode 1))))))
-
-;; (print (macroexpand-1 '(with-mode-off icomplete-vertical-mode  (find-file-at-point))))
-;; (print (macroexpand-1 '(with-mode-on icomplete-vertical-mode  (find-file-at-point))))
-(defmacro with-mode-on (mode &rest body)
-  (declare (indent defun)
-           (doc-string 3))
-  (macroexp-let2 nil mode-p `(bound-and-true-p ,mode)
-    `(progn
-       (unless ,mode-p (,mode 1))
-       (unwind-protect
-           (progn ,@body)
-         (unless ,mode-p (,mode -1))))))
-
-;; (print (macroexpand-1 '(icomplete-horizontal find-file  (find-file-at-point))))
-;; (icomplete-horizontal find-file  (find-file-at-point))
 (defmacro vmacs-defun (fun-name &rest body)
   (declare (indent defun)
            (doc-string 3))
@@ -58,28 +25,6 @@
     `(defun ,fun()
        (interactive)
        ,@body)))
-
-;; (global-set-key (kbd "C-c h") help-map)
-;; (define-key help-map (kbd "C-f") nil)
-;; (define-key help-map (kbd "C-m") nil)
-
-;; (defmacro vmacs-leader (key cmd)
-;;   `(global-set-key
-;;     (if (stringp ,key)
-;;         (kbd (concat "C-c " ,key))
-;;       (vconcat [3] ,key))               ;[3] =C-c ?
-;;     ,cmd))
-
-;; (defmacro vmacs-defun (fun-name &rest body)
-;;   (declare (indent defun)
-;;            (doc-string 3))
-;;   (let ((fun (intern (format "%s" fun-name))))
-;;     `(defun ,fun()
-;;        (interactive)
-;;        ,@body)))
-
-
-
 
 (provide 'conf-macro)
 
